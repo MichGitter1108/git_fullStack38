f91d12bf6ce94feb69316d2572aa5ae92e7529e0
michal mirchevsky <michalmi118@gmail.com>
2022-10-07 10:10:10 +0300

    changes

--- lessons/7.10- lesson 27- ES6/pointers/pointers.js
+++ lessons/7.10- lesson 27- ES6/pointers/pointers.js
@@ -10,6 +10,5 @@ for ( let item of arr )
     arr2.push(item);
 }
 
-console.log(arr, arr1, arr2);
-
+console.log(arr, arr1, arr2); // -> arr [1, 2, 3, 4, 5] arr1[1, 2, 3, 4, 5](same place of arr) arr2[1, 2, 3, 4, 5]
 
